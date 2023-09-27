4a5de5371b5e96a1fc37486170c1ab24d1b2c6ee
mrsaikumar-7 <pskyadav111@gmail.com>
2023-09-27 15:32:05 +0530

    The Virtual pen and Zoom features are working fine

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def quit(lmList, lmList2):
 
 
 # Parameters
-width, height = 1280, 720
+width, height = 1280, 1080
 gestureThreshold = 300
 
 folderPath = "Presentation"
@@ -32,7 +32,7 @@ detectorHand = HandDetector(detectionCon=0.8, maxHands=2)
 
 # Variables
 imgList = []
-delay = 20
+delay = 10
 buttonPressed = False
 counter = 0
 drawMode = False
@@ -76,8 +76,40 @@ while True:
 
         # Constrain values for easier drawing
         xVal = int(np.interp(lmList[8][0], [0,650], [0, width]))
-        yVal = int(np.interp(lmList[8][1], [150, height - 150], [0, height]))
+        yVal = int(np.interp(lmList[8][1], [100, height - 150], [0, height]))
+        # xVal = int(np.interp(lmList[8][0], [width // 2, width], [0, width]))
+        # yVal = int(np.interp(lmList[8][1], [150, height - 150], [0, height]))
         indexFinger = xVal, yVal
+        if len(allHands) == 2:
+            hand2 = allHands[1]
+            lmList2 = hand2['lmList']
+            fingers2 = detectorHand.fingersUp(hand2)
+            if fingers2 == [1, 1, 0, 0, 0] and fingers == [1, 1, 0, 0, 0]:
+                if (initialDistance is None):
+                    fin_distance, info, img = detectorHand.findDistance(lmList[8][0:2], lmList2[8][0:2], img)
+                    initialDistance = fin_distance
+                length, info, img = detectorHand.findDistance(lmList[8][0:2], lmList2[8][0:2], img)
+                print('initaial distance , length : ', initialDistance, length)
+                image_scale = int(1.8 * (length - initialDistance))
+                print('IMAGE SCALED BY ', image_scale)
+                cx, cy = info[4:]
+            # if quit(lmList, lmList2):
+            #     print('GESTURED QUIT is activated')
+            #     break
+        else:
+            initialDistance = None
+        try:
+            h1, w1, _ = imgCurrent.shape
+            # print('h1,w1', h1,w1)
+            # print('image_scale is : ',image_scale)
+            new_height, new_width = ((300 + 3 * image_scale) // 2) * 2, ((400 + 3 * image_scale) // 2) * 2
+            imgCurrent = cv2.resize(imgCurrent, (new_width, new_height))
+            # keeping the image in the center of the width
+            windowCenter = (0, 0)
+            cv2.moveWindow(winName, windowCenter[0], windowCenter[1])
+            cv2.resizeWindow(winName, new_width, new_height)
+        except:
+            pass
 
         if cy <= gestureThreshold:  # If hand is at the height of the face
             if fingers == [1, 0, 0, 0, 0]:
@@ -89,8 +121,8 @@ while True:
                     annotationNumber = -1
                     annotationStart = False
                 else:
-                    cv2.putText(imgCurrent, 'Oops, it is FIRST SLIDE', (20, 40), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                                fontScale=2, color=(256, 0, 0), lineType=cv2.LINE_AA, thickness=2)
+                    cv2.putText(imgCurrent, 'FIRST SLIDE', (20, 40), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                                fontScale=3, color=(256, 0, 0), lineType=cv2.LINE_AA, thickness=2)
             if fingers == [0, 0, 0, 0, 1]:
                 print("Right")
                 buttonPressed = True
@@ -100,18 +132,16 @@ while True:
                     annotationNumber = -1
                     annotationStart = False
                 else:
-                    cv2.putText(imgCurrent, 'Oops, it is LAST SLIDE', (20, 40), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                                fontScale=2, color=(256, 0, 0), lineType=cv2.LINE_AA, thickness=2)
-
+                    cv2.putText(imgCurrent, 'LAST SLIDE', (20, 40), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                                    fontScale=3, color=(256, 0, 0), lineType=cv2.LINE_AA, thickness=2)
         if fingers == [0, 1, 1, 0, 0]:
             cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
 
-        if fingers == [0, 1, 0, 0, 0]:
+        if ((fingers == [0, 1, 0, 0, 0] or fingers == [1,1,0,0,0]) and len(allHands)==1):
             if annotationStart is False:
                 annotationStart = True
                 annotationNumber += 1
                 annotations.append([])
-            print(annotationNumber)
             annotations[annotationNumber].append(indexFinger)
             cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
 
@@ -122,52 +152,19 @@ while True:
                 annotations.pop(-1)
                 annotationNumber -= 1
                 buttonPressed = True
-        else:
-            annotationStart = False
-
+    else:
+        annotationStart = False
     if buttonPressed:
         counter += 1
         if counter > delay:
             counter = 0
             buttonPressed = False
 
-    if len(allHands) == 2:
-        hand2 = allHands[1]
-        lmList2 = hand2['lmList']
-        fingers2 = detectorHand.fingersUp(hand2)
-
-        if fingers2 == [1, 1, 0, 0, 0] and fingers == [1, 1, 0, 0, 0]:
-            if (initialDistance is None):
-                fin_distance, info, img = detectorHand.findDistance(lmList[8][0:2], lmList2[8][0:2], img)
-                initialDistance = fin_distance
-            length, info, img = detectorHand.findDistance(lmList[8][0:2], lmList2[8][0:2], img)
-            print('initaial distance , length : ', initialDistance, length)
-            image_scale =  int(1.8 * (length - initialDistance))
-            print('IMAGE SCALED BY ', image_scale)
-            cx, cy = info[4:]
-
-        # if quit(lmList, lmList2):
-        #     print('GESTURED QUIT is activated')
-        #     break
-    else:
-        initialDistance = None
-    try:
-        h1, w1, _ = imgCurrent.shape
-        # print('h1,w1', h1,w1)
-        # print('image_scale is : ',image_scale)
-        new_height, new_width = ((300 + 2 * image_scale) // 2) * 2, ((400 + 2 * image_scale) // 2) * 2
-        imgCurrent = cv2.resize(imgCurrent, (new_width, new_height))
-        # keeping the image in the center of the width
-        windowCenter = (info[4] - new_width // 2, info[5])
-        cv2.moveWindow(winName, windowCenter[0], windowCenter[1])
-        cv2.resizeWindow(winName, new_width, new_height)
-    except:
-        pass
     for i, annotation in enumerate(annotations):
         for j in range(len(annotation)):
             if j != 0:
+                # print(annotation)
                 cv2.line(imgCurrent, annotation[j - 1], annotation[j], (0, 0, 200), 12)
-
     h, w, _ = imgCurrent.shape
     imgSmall = cv2.resize(img, (w // 6, h // 6))
     try:
